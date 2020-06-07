27ac539d558d2421667c7bebf3772081d4a20c1e
mahyarazad <m84123311@china.huawei.com>
2020-06-07 09:24:50 +0300

    Reshape Update

--- reshape.py
+++ reshape.py
@@ -61,38 +61,22 @@ class reshape:
 	@property
 	def dict_data_gen(self): 
 
-		city_data = reshape(self.df).south
-		city_data2 = reshape(self.df).north
-		city_data = pd.concat([city_data,city_data2])
+		city_data = pd.concat([reshape(self.df).south,reshape(self.df).north])
 		city_data.set_index(['Sell Out Province'],inplace = True)
-		col_list = []
-		deleteList = []
-		for col in range(0,int(city_data.shape[1])):
-		    col_list.append(str(city_data.columns[col])[0:10])
-		    deleteList.append(col)
-		city_data.columns = col_list
-		date_list = city_data.columns.values.tolist()
-		md = city_data.reset_index()
-		mdr = []
-		mdc = []
-		for rows in range(0,md.shape[0]):
-		    mdr = []
-		    for col in range(1,md.shape[1]):
-		        mdr.append(md.iloc[rows,col])
-		    mdc.append(list(mdr))
-		city_data['SO'] = mdc
-		city_data['Date'] = [date_list] * len(city_data)
-		city_data = city_data.drop(city_data.columns[deleteList], axis=1)
-		city_data.unstack()
-		city_data = city_data.transpose()
-		city_data = city_data.to_dict()
-		return city_data
+		dict_data_gen = {}
+		DateList = np.array([])
+		DateList = [str(col)[0:10] for col in city_data.columns]
+		for city in range(city_data.shape[0]):	
+			dict_data_gen.update({city_data.index[city]:dict(SO=np.array(city_data.iloc[city,:]),Date=DateList)})
+		return dict_data_gen
+		
 
 	@property
 	def summation(self):
 		self.df[self.df.columns[2]].replace(to_replace = 'NAZO GENERAL TRADING LLC',value= 'Global',inplace=True)
 		self.df[self.df.columns[2]].replace(to_replace = 'GLOBAL CO.FOR TECHNOLOGY,MOBILE DEVICES&ELECTRONIC APPLIANCES LTD',value= 'Global',inplace=True)
 		self.df[self.df.columns[2]].replace(to_replace = 'Euro Telecom FZE',value= 'Euro',inplace=True)
+		#### Adding Subtotal to the Base dataframe ####
 		collist = list(self.df.columns)
 		subtotalacc = pd.DataFrame()
 		subtotalacc = self.df.pivot_table(index = 
@@ -100,11 +84,11 @@ class reshape:
 			values=self.df.columns[-1],aggfunc=sum).reset_index()
 		subtotalacc['Product'],subtotalacc['Product Model'],subtotalacc['Product Family'] ='All','All','All'
 		subtotalacc = subtotalacc[collist]
-		df = pd.concat([subtotalacc,self.df])
-		df = df[df[df.columns[0]]=='Iraq']
-		df = df.sort_values(by = 'Sell Out Date')
-		df = df.pivot_table(index = [df.columns[1],df.columns[2],df.columns[3],df.columns[5]], columns = df.columns[-2], values = df.columns[-1], aggfunc = sum).reset_index()
-		return df
+		self.df = pd.concat([subtotalacc,self.df])
+		self.df = self.df[self.df[self.df.columns[0]]=='Iraq']
+		self.df = self.df.sort_values(by = 'Sell Out Date')
+		self.df = self.df.pivot_table(index = [self.df.columns[1],self.df.columns[2],self.df.columns[3],self.df.columns[5]], columns = self.df.columns[-2], values = self.df.columns[-1], aggfunc = sum).reset_index()
+		return self.df
 
 	@property
 	def geo(self):
@@ -120,6 +104,6 @@ class reshape:
 		cv = pd.merge(cv,df[[df.columns[0],df.columns[1],df.columns[2]]],on = df.columns[0],how = 'left')
 		cv = cv.rename(columns = {cv.columns[-1]:'Activation for ' + str(cv.columns[-1]),
 								  cv.columns[-2]:'Activation for ' + str(cv.columns[-2])})
+
 		return cv
-		
-		
+
